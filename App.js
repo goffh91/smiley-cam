8c98753809c8009dee0427d9a0173d789ab3d2ca
KyunWuPark <goffh91@gmail.com>
2019-07-16 08:29:43 +0900

    .

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-import { Camera, Permissions } from "expo";
+import { ActivityIndicator, Dimensions } from "react-native";
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+import { MaterialIcons } from '@expo/vector-icons';
+import styled from 'styled-components';
+
+const { width, height } = Dimensions.get("window");
+
+const CenterView = styled.View`
+  flex: 1;
+  margin-top: 40px;
+  justify-content: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 22px;
+`;
+
+const IconBar = styled.View`
+  margin-top: 50px;
+`;
 
 export default class App extends React.Component {
   state = {
-    hasPermission: null
+    hasPermission: null,
+    cameraType: Camera.Constants.Type.front
   };
   componentDidMount = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -15,21 +37,44 @@ export default class App extends React.Component {
     }
   };
   render() {
-    const { hasPermission } = this.state;
+    const { hasPermission, cameraType } = this.state;
     if (hasPermission === true) {
       return (
-        <View>
-          <Text>Has permissions</Text>
-        </View>
+        <CenterView>
+          <Camera 
+            style={{
+              width,
+              height: height - 40,
+              borderRadius: 10,
+              overflow: 'hidden'
+            }} 
+            type={cameraType}
+          />
+          <IconBar>
+            <MaterialIcons
+              name={cameraType === Camera.Constants.Type.front ? 
+                'camera-rear' : 'camera-front' }
+              color="white"
+              size={30}
+            />
+          </IconBar>
+        </CenterView>
       );
     } else if (hasPermission === false) {
       return (
-        <View>
+        <CenterView>
           <Text>Don't have permission for this</Text>
-        </View>
+        </CenterView>
       );
     } else {
-      return <ActivityIndicator />;
+      return (
+        <CenterView>
+          <ActivityIndicator 
+            size={'large'}
+            color={'black'}
+          />
+        </CenterView>
+      );
     }
   }
 }
