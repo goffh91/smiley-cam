f6f4455765facd67a2d4fe56a46113e1c8f7ea14
KyunWuPark <goffh91@gmail.com>
2019-07-18 01:43:56 +0900

    .

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { ActivityIndicator, Dimensions } from "react-native";
-import * as Permissions from 'expo-permissions';
+import { FileSystem } from 'expo';
+import { ActivityIndicator, Dimensions, TouchableOpacity } from "react-native";
 import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import * as FaceDetector from 'expo-face-detector';
 import { MaterialIcons } from '@expo/vector-icons';
 import styled from 'styled-components';
 
@@ -9,24 +11,35 @@ const { width, height } = Dimensions.get("window");
 
 const CenterView = styled.View`
   flex: 1;
-  margin-top: 40px;
   justify-content: center;
-  background-color: cornflowerblue;
+  align-items: center;
+  background-color: whitesmoke;
 `;
 
 const Text = styled.Text`
   color: white;
+  margin-top: 10px;
   font-size: 22px;
 `;
 
 const IconBar = styled.View`
-  margin-top: 50px;
+  margin-top: 20px;
 `;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasPermission: null,
+      cameraType: Camera.Constants.Type.front,
+      smileDetected: null
+    };
+    this.cameraRef = React.createRef();
+  }
   state = {
     hasPermission: null,
-    cameraType: Camera.Constants.Type.front
+    cameraType: Camera.Constants.Type.front,
+    smileDetected: null
   };
   componentDidMount = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -37,26 +50,37 @@ export default class App extends React.Component {
     }
   };
   render() {
-    const { hasPermission, cameraType } = this.state;
+    const { hasPermission, cameraType, smileDetected } = this.state;
     if (hasPermission === true) {
       return (
         <CenterView>
           <Camera 
             style={{
               width,
-              height: height - 40,
-              borderRadius: 10,
+              height: height - 200,
+              borderRadius: 2,
               overflow: 'hidden'
             }} 
             type={cameraType}
+            onFacesDetected={smileDetected ? null : this.onFacesDetected}
+            faceDetectorSettings={{
+              detectLandmarks: FaceDetector.Constants.Landmarks.all,
+              runClassifications: FaceDetector.Constants.Classifications.all
+            }}
+            faceDetectionClassifications="all"
+            ref={this.cameraRef}
           />
           <IconBar>
-            <MaterialIcons
-              name={cameraType === Camera.Constants.Type.front ? 
-                'camera-rear' : 'camera-front' }
-              color="white"
-              size={30}
-            />
+            <TouchableOpacity onPress={this.switchCameraType}>
+              <MaterialIcons
+                name={cameraType === Camera.Constants.Type.front 
+                  ? 'camera-rear' 
+                  : 'camera-front'
+                }
+                color="gray"
+                size={40}
+              />
+            </TouchableOpacity>
           </IconBar>
         </CenterView>
       );
@@ -77,4 +101,50 @@ export default class App extends React.Component {
       );
     }
   }
+  switchCameraType = () => {
+    const { cameraType } = this.state;
+    if (cameraType === Camera.Constants.Type.front) {
+      this.setState({
+        cameraType: Camera.Constants.Type.back
+      });
+    } else {
+      this.setState({
+        cameraType: Camera.Constants.Type.front
+      });
+    }
+  }
+  onFacesDetected = ({ faces }) => {
+    const face = faces[0];
+    if (face) {
+      console.log(face.smilingProbability);
+      if (face.smilingProbability > 0.7) {
+        console.log(face.smilingProbability + ' : take photo');
+        this.setState({
+          smileDetected: true
+        });
+        this.takePhoto();
+      }
+    }
+  }
+  takePhoto = async () => {
+    try {
+      if (this.cameraRef.current) {
+        let { uri } = await this.cameraRef.current.takePictureAsync({
+          quality: 1,
+          exif: true
+        });
+        if (uri) {
+          this.savePhoto(uri);
+        }
+      }
+    } catch(error) {
+      alert(error);
+      this.setState({
+        smileDetected: false
+      })
+    }
+  }
+  savePhoto = async(uri) => {
+    
+  } 
 }
